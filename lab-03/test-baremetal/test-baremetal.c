90659bc3744ea1b2cb512d37b101dd925fd87503
Mladen Knezic <mladen.knezic@etf.unibl.org>
2024-03-29 12:49:47 +0100

    Added source files for the first example in lab-03

---
+++ lab-03/test-baremetal/test-baremetal.c
@@ -0,0 +1,33 @@
+/*
+===========================================================
+Name 		: test-baremetal.c
+Author 		: Mladen Knezic
+Version 	: 0.1
+Copyright 	: BSD License
+Description	: Simple Cortex A9 bare-metal C program
+===========================================================
+*/
+
+int counter = 0;
+int result;
+
+int main(void)
+{
+	int a = 4;
+	int b = 5;
+
+	result = 0;
+
+	while (1)
+	{
+		if (++counter > 10)
+		{
+			result += a + b;
+			a += 2;
+			b += 3;
+			counter = 0;
+		}
+	}
+
+	return 0;
+}
