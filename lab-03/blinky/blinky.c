5f2d69d8ff0af21a2672cb52b0ebbb324606009a
Chameleeon <danichelsea3@gmail.com>
2025-04-09 12:40:32 +0200

    Pressing the button will now toggle whether LED blinking is paused or not

--- lab-03/blinky/blinky.c
+++ lab-03/blinky/blinky.c
@@ -13,6 +13,7 @@ Description	: HPS_LED blinking example C program
 
 // Bit position of the HPS LED pin in the GPIO1 module
 #define HPS_LED		(24U)
+#define HPS_KEY		(25U)
 
 // Busy-wait function
 static inline void spin(volatile int count)
@@ -27,21 +28,38 @@ int main(void)
 {
 	// HPS LED initial state is OFF
 	unsigned int led_state = 0;
+	unsigned int is_paused = 0;
+	unsigned int prev_button_state = 1;
 
 	// Initialize the board (IOCSR, pinmux, and reset)
 	board_init();
 
 	// Set HPS LED as output pin
 	set_gpio_dir(GPIO_OUTPUT, HPS_LED);
+	set_gpio_dir(GPIO_INPUT, HPS_KEY);
 
 	while (1)
 	{
-		// Update HPS LED state
-		write_gpio(HPS_LED, led_state);
-		// Wait for approximately 0.5s
-		spin(250000);
-		// Toggle the LED state
-		led_state ^= 1;
+		// register input
+		unsigned int button_state = read_gpio(HPS_KEY);
+		if(button_state != 1 && prev_button_state != 0)
+		{
+			prev_button_state = 0;
+			is_paused ^= 1;
+		}
+		if(button_state != 0 && prev_button_state == 0){
+			prev_button_state = 1;
+		}
+
+		if(!is_paused){
+			// Update HPS LED state
+			write_gpio(HPS_LED, led_state);
+			// Wait for approximately 0.5s
+			spin(250000);
+			// Toggle the LED state
+			led_state ^= 1;
+		}
+
 	}
 
 	return 0;
