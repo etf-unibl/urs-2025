7f355c859286a0904eb754fd694fb80c8878334b
Chameleeon <danichelsea3@gmail.com>
2025-05-14 12:19:10 +0200

    Enabled LED blinking from user space in blinky.c. Enabled button toggling for the blinking in blinky-button.c

--- lab-07/blinky/blinky-button.c
+++ lab-07/blinky/blinky-button.c
@@ -8,6 +8,7 @@ Description	: Linux HPS_LED blinking example C program
 *******************************************************************************
 */
 
+#include <pthread.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -17,13 +18,24 @@ Description	: Linux HPS_LED blinking example C program
 #define GPIO1_REGS_SIZE         (0x00001000)
 #define GPIO1_REGS_DATA_OFFSET  (0x0)
 #define GPIO1_REGS_DIR_OFFSET   (0x4)
+#define GPIO1_DEBOUNCE_OFFSET 	(0x48)
+#define GPIO1_REGS_IN_OFFSET	(0x50)
 
 #define HPS_LED_PIN	        (24U)
+#define HPS_KEY_PIN		(25U)
+
+unsigned int blinkingEnabled = 0;
+pthread_mutex_t threadMutex = PTHREAD_MUTEX_INITIALIZER;
+
+void* read_button(void* arg);
+
+volatile unsigned int* hps_led_dir_addr = NULL;
+volatile unsigned int* hps_led_port_addr = NULL;
+volatile unsigned int* hps_key_port_addr = NULL;
+volatile unsigned int* debounce_addr = NULL;
 
 int main(void)
 {
-    volatile unsigned int* hps_led_dir_addr = NULL;
-    volatile unsigned int* hps_led_port_addr = NULL;
     void* virtual_base;
     int fd;
 
@@ -50,26 +62,45 @@ int main(void)
     hps_led_dir_addr = (unsigned int *)(virtual_base + GPIO1_REGS_DIR_OFFSET);
     // Get the address that maps to the HPS_LED port register
     hps_led_port_addr = (unsigned int *)(virtual_base + GPIO1_REGS_DATA_OFFSET);
+    hps_key_port_addr = (unsigned int *)(virtual_base + GPIO1_REGS_IN_OFFSET);
+
+    debounce_addr = (unsigned int *)(virtual_base + GPIO1_DEBOUNCE_OFFSET);
     
     // Set HPS_LED direction to be output
-    *hps_led_dir_addr = 1 << 24;
+    *hps_led_dir_addr |= 1 << HPS_LED_PIN;
     
     // Turn the HPS_LED off initially
-    *hps_led_port_addr = 1 << 24;
+    *hps_led_port_addr |= 1 << HPS_LED_PIN;
 
+    // Set HPS_KEY direction to be input
+    *hps_led_dir_addr &= ~(1 << HPS_KEY_PIN);
+    *debounce_addr &= ~(1 << HPS_KEY_PIN);
+
+
+    pthread_t buttonThread;
+    if(pthread_create(&buttonThread, NULL, read_button, NULL) != 0)
+    {
+	    perror("Thread creation failed");
+	    return 1;
+    }
     // Loop infinitely and toggle the HPS_LED every 0.5 seconds
     while(1)
     {
-	    int state = *hps_led_port_addr >> 24 & 1;
+	    pthread_mutex_lock(&threadMutex);
+	    if(blinkingEnabled)
+	    {
+		    int state = *hps_led_port_addr >> HPS_LED_PIN & 1;
+		    if(state == 0)
+		    {
+			    *hps_led_port_addr |= 1 << HPS_LED_PIN;
+		    }
+		    else
+		    {
+			    *hps_led_port_addr &= ~(1 << HPS_LED_PIN);
+		    }
+	    }
+	    pthread_mutex_unlock(&threadMutex);
 	    usleep(500000);
-	    if(state == 0)
-	    {
-		    *hps_led_port_addr |= 1 << 24;
-	    }
-	    else
-	    {
-		    *hps_led_port_addr &= 0 << 24;
-	    }
     }
 
     // Unmap previously mapped virtual address space
@@ -86,3 +117,25 @@ int main(void)
     return 0;
 }
 
+
+void* read_button(void* arg)
+{
+	int prevButtonState = 1;
+	while(1)
+	{
+		pthread_mutex_lock(&threadMutex);
+		unsigned int buttonState = *hps_key_port_addr & (1 << HPS_KEY_PIN);
+
+		if(prevButtonState != 0 && buttonState == 0)
+		{
+			blinkingEnabled = ~blinkingEnabled;
+			prevButtonState = 0;
+		}
+		else if (buttonState && prevButtonState == 0)
+		{
+			prevButtonState = 1;
+		}
+		pthread_mutex_unlock(&threadMutex);
+		usleep(2000);
+	}
+}
