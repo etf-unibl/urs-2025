526d8b046d3afa4fb896469fae2f8f067bfc3507
Luka Zeljko <luka@lukazeljko.xyz>
2026-05-15 14:02:19 +0200

    Lab 7 all tasks
    - Modified blinky example
    - Added threaded blinky button program
    - Added defconfig
    - Added device tree for socfpga_cyclone5_de1_soc

--- lab-07/blinky-button/blinky-button.c
+++ lab-07/blinky-button/blinky-button.c
@@ -12,20 +12,34 @@ Description	: Linux HPS_LED blinking example C program
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/mman.h>
+#include <pthread.h>
 
 #define GPIO1_REGS_BASE         (0xff709000)
 #define GPIO1_REGS_SIZE         (0x00001000)
 #define GPIO1_REGS_DATA_OFFSET  (0x0)
 #define GPIO1_REGS_DIR_OFFSET   (0x4)
+#define GPIO1_REGS_EXT_OFFSET   (0x50)
 
 #define HPS_LED_PIN	        (24U)
+#define HPS_KEY_PIN	        (25U)
+
+volatile unsigned int* hps_led_dir_addr = NULL;
+volatile unsigned int* hps_led_port_addr = NULL;
+volatile unsigned int* hps_ext_port_addr = NULL;
+
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+int blink_enabled = 1;
+
+void *check_button();
+void *toggle_led();
 
 int main(void)
 {
-    volatile unsigned int* hps_led_dir_addr = NULL;
-    volatile unsigned int* hps_led_port_addr = NULL;
     void* virtual_base;
     int fd;
+
+	pthread_t thread1, thread2;
+	int iret1, iret2;
 
     // Open /dev/mem
     fd = open("/dev/mem", (O_RDWR | O_SYNC));
@@ -50,16 +64,21 @@ int main(void)
     hps_led_dir_addr = (unsigned int *)(virtual_base + GPIO1_REGS_DIR_OFFSET);
     // Get the address that maps to the HPS_LED port register
     hps_led_port_addr = (unsigned int *)(virtual_base + GPIO1_REGS_DATA_OFFSET);
+
+    hps_ext_port_addr = (unsigned int *)(virtual_base + GPIO1_REGS_EXT_OFFSET);
     
-       *hps_led_dir_addr |= (1 << HPS_LED_PIN);
-       *hps_led_port_addr &= ~(1 << HPS_LED_PIN);
+	*hps_led_dir_addr &= ~(1 << HPS_KEY_PIN);
+	*hps_led_dir_addr |= (1 << HPS_LED_PIN);
+	*hps_led_port_addr &= ~(1 << HPS_LED_PIN);
+
+	printf("%X\n", *hps_led_dir_addr);
+	fflush(stdout);
     
-    // Loop infinitely and toggle the HPS_LED every 0.5 seconds
-    while(1)
-    {
-               *hps_led_port_addr ^= (1U << HPS_LED_PIN);
-               usleep(500000);
-    }
+	iret1 = pthread_create(&thread1, NULL, check_button, NULL);
+	iret1 = pthread_create(&thread1, NULL, toggle_led, NULL);
+
+	pthread_join(thread1, NULL);
+	pthread_join(thread2, NULL);
 
     // Unmap previously mapped virtual address space
     if(0 != munmap(virtual_base, GPIO1_REGS_SIZE))
@@ -75,3 +94,27 @@ int main(void)
     return 0;
 }
 
+void *check_button() {
+	while(1) {
+		if((*hps_ext_port_addr & (1 << HPS_KEY_PIN)) != (1 << HPS_KEY_PIN)) {
+			usleep(100000);
+			if((*hps_ext_port_addr & (1 << HPS_KEY_PIN)) != (1 << HPS_KEY_PIN)) {
+
+				pthread_mutex_lock(&mutex1);
+					if(blink_enabled == 0)
+						blink_enabled = 1;
+					else blink_enabled = 0;
+				pthread_mutex_unlock(&mutex1);
+			}
+		}
+	}
+}
+
+void *toggle_led() {
+	while(1) {
+		if(blink_enabled) {
+			*hps_led_port_addr ^= (1U << HPS_LED_PIN);
+			usleep(500000);
+		}
+	}
+}
